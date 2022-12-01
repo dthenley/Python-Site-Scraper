3337393d248800e66de76979d1445eeb43981810
Donte <3503886+dthenley@users.noreply.github.com>
2022-12-01 13:31:24 -0600

    remove test site

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-url = "https://www.dahlkefinancialks.com/learning_center/research/"
+url = {blog_url}
 page = requests.get(url)
 page = page.content
 
@@ -16,7 +16,7 @@ for article in articles:
     title = article.find('a').text
     excerpt = article.find('p').text
     link = article.find('a').attrs['href']
-    newurl = f"https://www.dahlkefinancialks.com{link}"
+    newurl = f"site_url{link}"
     newpage = requests.get(newurl)
     newsoup = BeautifulSoup(newpage.content, 'html.parser')
     articleContent = newsoup.find('div', {'id': 'Content_container'})
